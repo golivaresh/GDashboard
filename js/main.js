e32d02c5458740eda7c0fa05133331716ac50e67
Gustavo Olivares Hernandez <golivaresh.dev@gmail.com>
2021-03-28 18:38:42 -0600

    MOD Header customization. By: golivaresh

--- js/main.js
+++ js/main.js
@@ -2,9 +2,7 @@
 // Ready
 $(document).ready(function () {
     saveResizeElements();
-    let colour = 'red';
-    document.body.className = '';
-    document.body.classList.add(`theme-${ colour }`);
+    changeTheme( localStorage.getItem( 'g-theme' ) );
 });
 /* *************************************************************************************************** */
 /* *************************************************************************************************** */
@@ -23,17 +21,9 @@ const resizeRootVariable = ( varName, value ) => {
 };
 
 const changeTheme = ( theme ) => {
+    theme = ( !theme ) ? 'red' : theme;
     document.body.className = '';
     document.body.classList.add(`theme-${ theme }`);
     localStorage.setItem('g-theme', theme);
 }
-// // Add slideDown animation to Bootstrap dropdown when expanding.
-// $('.dropdown').on('show.bs.dropdown', function() {
-//     $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
-//   });
-
-//   // Add slideUp animation to Bootstrap dropdown when collapsing.
-//   $('.dropdown').on('hide.bs.dropdown', function() {
-//     $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
-//   });
 /* *************************************************************************************************** */
