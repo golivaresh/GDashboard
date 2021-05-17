468c469afb4586d6e166d9e8ac4c74fb63d65e8e
Gustavo Olivares Hernandez <golivaresh.dev@gmail.com>
2021-05-16 20:20:07 -0500

    MOD Bootstrap library was modified, the main script was offuscated. By: golivaresh

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 /* *************************************************************************************************** */
-// Variables
+/* ***************************************** Global Variables **************************************** */
+/* *************************************************************************************************** */
 // Sidebar
 let sidebarBg;
 let sidebar;
@@ -14,15 +15,29 @@ let sidebarAccordionFlush;
 let actionSection;
 let actionSectionElements;
 let btnCloseActionSection;
-// Login
-// let divLogin;
-// let divFormLogin;
-// let divFormSignIn;
-// let btnToggleLoginDiv;
 /* *************************************************************************************************** */
+
 /* *************************************************************************************************** */
-// Ready
-$(document).ready(function () {
+/* ******************************************** Constants ******************************************** */
+/* *************************************************************************************************** */
+// Header
+const CSS_SIDEBAR_BG = {
+    'background': '',
+    'background-repeat': 'no-repeat',
+    'background-position': 'center',
+    'background-size': 'cover',
+};
+/* *************************************************************************************************** */
+
+/* *************************************************************************************************** */
+/* ********************************************** Ready ********************************************** */
+/* *************************************************************************************************** */
+/**
+ * Set the variables.
+ * 
+ * @returns void.
+ */
+const setComponents = () => {
     // Sidebar
     mainContainer = $( '#mainContainer' );
     sidebar = $( '#sidebar' );
@@ -38,51 +53,211 @@ $(document).ready(function () {
     actionSection = $('#actionSection');
     btnCloseActionSection = $('#btnCloseActionSection');
     actionSectionElements = $('#actionSection>div#actionInput>div');
-    // Login
-    // divLogin = $('#divLogin');
-    // divFormLogin = $('#divFormLogin');
-    // divFormSignIn = $('#divFormSignIn');
-    // btnToggleLoginDiv = $('.btn-toggle-login');
-
-    saveResizeElements();
-    changeTheme( localStorage.getItem( 'g-theme' ) );
-
-    // btnToggleLoginDiv.click(function (e) { 
-    //     divFormLogin.toggleClass( 'show' );
-    //     divFormSignIn.toggleClass( 'show' );
-    //     divLogin.toggleClass( 'show-sign-in' );
-    //     e.preventDefault();
-    // });
-});
+};
+/**
+ * Set component events.
+ */
+const setEvents = () => {
+    $( window ).resize(function() {
+        saveResizeElements();
+    });
+    btnMenuToggle.click( (e) => { 
+        toggleMenu();
+        e.preventDefault();
+    });
+    sidebarProfileOpc.click( (e) => {
+        clickInSmallScreen( e );
+        e.preventDefault();
+    });
+    sidebarAccordionOpc.click( (e) => {
+        clickInSmallScreen( e );
+        e.preventDefault();
+    });
+    sidebar.hover(
+        () => { // Mouseenter event.
+        }, () => { // Mouseleave event.
+            // If the sidebar is not expanded, hide the accordion.
+            if ( !sidebar.hasClass('m-expanded') ) {
+                hideAccordion();
+            }
+        }
+    );
+};
+/**
+ * Check if we are in a small screen and the selected button does not have the "active" class.
+ * If this si true it doesn't do any action, otherwise close the menu.
+ * 
+ * @param {Event} e Event emmited.
+ * @returns void.
+ */
+ const clickInSmallScreen = ( e ) => {
+    if ( isSmallScreen() && !$(e.target || e.toElement).hasClass('active') ) {
+        setTimeout(() => {
+            toggleMenu();
+        }, 100);
+    }
+};
 /* *************************************************************************************************** */
+
 /* *************************************************************************************************** */
-// Resize
-$( window ).resize(function() {
-    saveResizeElements();
-});
-function saveResizeElements() {
-    $(':root').css('--window-height', $( window ).height() );
-    $(':root').css('--window-height-px', ( $( window ).height() + 'px') );
-    $(':root').css('--window-width', $(window).width() );
-    $(':root').css('--window-width-px', ( $(window).width() + 'px') );
-    // $(':root').css('--div-login-height-px', ( '-' + divFormLogin.height() + 'px') );
-}
-const resizeRootVariable = ( varName, value ) => {
+/* ****************************************** Configuration ****************************************** */
+/* *************************************************************************************************** */
+/**
+ * Toggle classes of menu.
+ * 
+ * @returns void.
+ */
+ const toggleMenu = () => {
+    // Toggle manu classes.
+    sidebar.toggleClass( 'm-collapsed' );
+    sidebar.toggleClass( 'm-expanded' );
+    mainContainer.toggleClass('m-main-collapsed');
+    mainContainer.toggleClass('m-main-expanded');
+    sidebarOpc.toggleClass( 'm-opc-collapsed' );
+    sidebarOpc.toggleClass( 'm-opc-expanded' );
+    btnMenuToggle.toggleClass( 'btn-menu-collapsed' );
+    btnMenuToggle.toggleClass( 'btn-menu-expanded' );
+    
+    if ( !sidebar.hasClass('m-expanded') ) {
+        // If menu is not expanded, hide the accordion.
+        hideAccordion();
+    } else {
+        // If menu is expanded, expande the active accordion option.
+        expandedIfActive();
+    }
+};
+/**
+ * Expande Menu option if submenu is active.
+ * 
+ * @returns void.
+ */
+ const expandedIfActive = () => {
+    // Obtains the active option.
+    const activeOpc = $('#sidebarAccordion>div.accordion-item>div.accordion-collapse>ul>li.active');
+    // Validate if any active option was obteined.
+    if ( activeOpc && activeOpc.length > 0 ) {
+        // Show the accordion parent.
+        $( `#${activeOpc[0].parentElement.parentElement.id}` ).collapse('show');
+    }
+};
+/**
+ * Hide accordion of menu.
+ * 
+ * @returns void.
+ */
+const hideAccordion = () => {
+    sidebarAccordionFlush.collapse('hide');
+};
+/* *************************************************************************************************** */
+
+/* *************************************************************************************************** */
+/* ****************************************** Configuration ****************************************** */
+/* *************************************************************************************************** */
+/**
+ * Set the indicated theme.
+ * 
+ * @param {string} theme Name theme to set.
+ * If this parameter is empty, it sets the "red theme" by default.
+ * @returns void
+ */
+ const setTheme = ( theme ) => {
+    theme = theme || 'red';
+    document.body.className = '';
+    document.body.classList.add( `theme-${ theme }` );
+    localStorage.setItem( 'g-theme', theme );
+};
+/**
+ * Change de background sidebar.
+ * 
+ * @param {string} img Background image to be set on the sidebar. If parameter "img" is null, it sets "no-image-bg.jpg" by default.
+ * @returns void.
+ */
+ const changeSidebarBg = ( img ) => {
+    img = img || './img/sidebar/no-image-bg.jpg';
+    // Save current backgound.
+    localStorage.setItem( 'sidebarImgUrl', img );
+    CSS_SIDEBAR_BG.background = `url(${ img })`;
+    sidebarBg.css(CSS_SIDEBAR_BG);
+};
+/* *************************************************************************************************** */
+
+/* *************************************************************************************************** */
+/* ********************************************** Resize ********************************************* */
+/* *************************************************************************************************** */
+/**
+ * Saves height and width of the windows in "root" variables.
+ * 
+ * @return void
+ */
+const saveResizeElements = () => {
+    saveRootVariable( '--window-height', $( window ).height() );
+    saveRootVariable( '--window-height-px', ( $( window ).height() + 'px') );
+    saveRootVariable( '--window-width', $(window).width() );
+    saveRootVariable( '--window-width-px', ( $(window).width() + 'px') );
+};
+/**
+ * Save value in ":root" variable.
+ * 
+ * @param {string} varName Variable name.
+ * @param {string} value Value to save.
+ * @returns void
+ */
+const saveRootVariable = ( varName, value ) => {
     $(':root').css( varName, value );
 };
+/**
+ * Check if current screen is small.
+ * 
+ * @returns boolean If screen width less than 768px returns true, false otherwise.
+ */
+ const isSmallScreen = () => {
+    return ( $(window).width() < 768 );
+};
+/* *************************************************************************************************** */
 
-const changeTheme = ( theme ) => {
-    theme = ( !theme ) ? 'red' : theme;
-    document.body.className = '';
-    document.body.classList.add(`theme-${ theme }`);
-    localStorage.setItem('g-theme', theme);
-}
 /* *************************************************************************************************** */
-// Bootstrap componentes
+/* ********************************************** Header ********************************************* */
+/* *************************************************************************************************** */
+/**
+ * Open/close "action section".
+ * 
+ * @param {string} inputId id of the element tto show
+ * @returns void.
+ */
+ const toggleActionSection = ( inputId ) => {
+    actionSection.toggle( 'fast', () => {} );
+    btnCloseActionSection.toggle( 'fast', () => {} );
+    const input = $( `#${inputId}` );
+    
+    if ( input && input.hasClass('inactive') ) { // if the input is inactive.
+        // Remove "inactive" class.
+        input.toggleClass('inactive');
+    } else {
+        // Close the "action section".
+        actionSectionElements.addClass('inactive');
+    }
+};
+/**
+ * Clean input element.
+ * 
+ * @param {string} inputId id of the element to clean.
+ * @param {string} defaultValue value to set in the element.
+ * @returns void.
+ */
+const cleanElement = ( inputId, defaultValue ) => {
+    const inputElem = $( `#${ inputId }` );
+    if ( inputElem && inputElem.length > 0 ) {
+        inputElem[0].value = ( defaultValue || '' );
+    }
+};
+/* *************************************************************************************************** */
+
+/* *************************************************************************************************** */
+/* ************************************** Bootstrap components *************************************** */
+/* *************************************************************************************************** */
 // Popovers.
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
-/* *************************************************************************************************** */
+const popoverTriggerList = [].slice.call( document.querySelectorAll( '[data-bs-toggle="popover"]' ) );
+const popoverList = popoverTriggerList.map( function ( popoverTriggerEl ) {
+  return new bootstrap.Popover( popoverTriggerEl );
+});
 /* *************************************************************************************************** */
